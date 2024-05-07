8e76f590984a5aeae3e3a774b237e0eea6cff0b1
aipoool <comdtvise@gmail.com>
2024-05-07 18:26:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,6 +26,17 @@ const app = express();
 // }));
 app.use(cors()); 
 
+app.use(session({
+    secret: process.env.SECRET_SESSION,
+    resave: false, //we dont want to save a session if nothing is modified
+    saveUninitialized: false, //dont create a session until something is stored
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      secure: true, //Enable when deployment OR when not using localhost, this wont work without https
+      sameSite: "none", //Enable when deployment OR when not using localhost, We're not on the same site, we're using different site so the cookie need to effectively transfer from Backend to Frontend
+    },
+  }));
+
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan("dev")); 
 }
