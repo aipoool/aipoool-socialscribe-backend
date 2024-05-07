2c545abf5bca9fa8e271f9be80b9e6be418a2568
aipoool <comdtvise@gmail.com>
2024-05-07 15:23:31 +0530

    Update authentication.js

--- routes/authentication.js
+++ routes/authentication.js
@@ -35,7 +35,7 @@ router.post("/userdata", async (req, res) => {
 });
 
 /**GENERAL BACKEND ROUTES */
-router.get("/login/success", async (req, res) => {
+router.post("/login/success", async (req, res) => {
   console.log("Request data from login/success : ", req.user); 
   if(req.user){
       res.status(200).json({message: "User Login" , user:req.user});
