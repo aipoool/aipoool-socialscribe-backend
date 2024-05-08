568c305786c79fe926581bf8d1d683e6e414b0bf
aipoool <comdtvise@gmail.com>
2024-05-08 23:14:33 +0530

    Update authentication.js

--- routes/authentication.js
+++ routes/authentication.js
@@ -22,7 +22,8 @@ router.get("/google/callback", passport.authenticate("google",
 router.post("/login/success", async (req, res) => {
   console.log("Request data from login/success : ", req.user); 
   if(req.user){
-      res.status(200).json({message: "User Login" , user:req.user});
+      //res.status(200).json({message: "User Login" , user:req.user});
+      console.log('Entered the login success route');
   }
   else{
       res.status(403).json({message: "User Not Authorized"});
