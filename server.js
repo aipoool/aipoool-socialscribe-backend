c5760f2249013b82878cfc563be871c18c865d75
aipoool <comdtvise@gmail.com>
2024-05-09 10:44:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ passport.deserializeUser((id, done)=>{
   app.get("/auth/login/success", async (req, res) => {
     console.log("Request data from login/success : ", req.user); 
     if(req.user){
-        //res.status(200).json({message: "User Login" , user:req.user});
+        res.status(200).json({message: "User Login" , user:req.user});
         console.log('Entered the login success route');
     }
     else{
