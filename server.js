712e818741a37502b548e0f5ae72d9118667133b
aipoool <comdtvise@gmail.com>
2024-05-15 20:01:08 +0530

    chore: Updated the extension ID

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ await connectionToDB();
 
 const app = express();
 app.use(cors({
-    origin: ['http://localhost:3000','https://socialscribe-aipoool.onrender.com','chrome-extension://dnjmipaneoddchfeamgdabpiomihncii'],
+    origin: ['http://localhost:3000','https://socialscribe-aipoool.onrender.com','chrome-extension://bhnpbgfnodkiohanbolcdkibeibncobf'],
     methods: ['GET','PUT','POST', 'DELETE','OPTIONS'],
     credentials: true
 }));
