50746ef3766fbf577e62c3edf466272b68eafea6
aipoool <comdtvise@gmail.com>
2024-07-13 14:57:52 +0530

    Updating the isauthenticated() function - 5

--- server.js
+++ server.js
@@ -41,30 +41,34 @@ app.use("/stripe-webhook", express.raw({ type: "application/json" }));
 app.use(express.json());
 
 app.set("trust proxy", 1);
-// app.use(
-//   session({
-//     secret: process.env.SECRET_SESSION,
-//     resave: true, //we dont want to save a session if nothing is modified
-//     saveUninitialized: false, //dont create a session until something is stored
-//     cookie: {
-//       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-//       secure: "auto",
-//       sameSite: "none", //Enable when deployment OR when not using localhost, We're not on the same site, we're using different site so the cookie need to effectively transfer from Backend to Frontend
-//     },
-//   })
-// );
-
 app.use(
   session({
     secret: process.env.SECRET_SESSION,
     resave: true, //we dont want to save a session if nothing is modified
-    saveUninitialized: true, //dont create a session until something is stored
+    saveUninitialized: false, //dont create a session until something is stored
     store: new MongoStore({
       mongoUrl: process.env.DATABASE,
       collection: 'sessions'
     })
+    // cookie: {
+    //   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    //   secure: "auto",
+    //   sameSite: "none", //Enable when deployment OR when not using localhost, We're not on the same site, we're using different site so the cookie need to effectively transfer from Backend to Frontend
+    // },
   })
 );
+
+// app.use(
+//   session({
+//     secret: process.env.SECRET_SESSION,
+//     resave: true, //we dont want to save a session if nothing is modified
+//     saveUninitialized: true, //dont create a session until something is stored
+//     store: new MongoStore({
+//       mongoUrl: process.env.DATABASE,
+//       collection: 'sessions'
+//     })
+//   })
+// );
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
